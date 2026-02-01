0e2be5ec528f624af1abd818c7899e2493048271
Runyi927 <runyi.ye@esmt.berlin>
2026-02-01 14:59:33 +0000

    fix: adding pos_label to baseline.evaluate()

--- baseline.py
+++ baseline.py
@@ -1,8 +1,10 @@
 import numpy as np
-import pandas as pd
 from sklearn.metrics import accuracy_score, f1_score
 
 class BaselineModel:
+    """
+    A simple baseline model that predicts the most frequent class.
+    """
     def __init__(self):
         self.most_frequent_class = None
 
@@ -12,9 +14,9 @@ class BaselineModel:
     def predict(self, X):
         return np.full(X.shape[0], self.most_frequent_class)
 
-    def evaluate(self, X, y):
+    def evaluate(self, X, y, pos_label='t'):  
         y_pred = self.predict(X)
         return {
             "accuracy": accuracy_score(y, y_pred),
-            "f1_score": f1_score(y, y_pred, pos_label='true')  
+            "f1_score": f1_score(y, y_pred, pos_label=pos_label)
         }
